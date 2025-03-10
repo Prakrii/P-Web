31f09b6d4886e7e2caf6728d5297f5d0be9700bf
Prakrit Acharya <prakrit.bss@gmail.com>
2025-03-10 14:34:49 +0545

    changes made

--- main.js
+++ main.js
@@ -1,16 +1,47 @@
+console.log("a")
+//text effect animation
+const containerEl = document.querySelector(".container");
+
+const careers = ["Web Developer", "Designer", "Freelancer", "Student"];
+
+let careerIndex = 0;
+
+let characterIndex = 0;
+
+updateText();
+
+function updateText() {
+  characterIndex++;
+  containerEl.innerHTML = `
+    <h3>And I'm ${careers[careerIndex].slice(0, 1) === "I" ? "an" : "a"} <span>${careers[
+    careerIndex ].slice(0, characterIndex)}_</span></h3>
+    `;
+
+  if (characterIndex === careers[careerIndex].length) {
+    careerIndex++;
+    characterIndex = 0;
+  }
+
+  if (careerIndex === careers.length) {
+    careerIndex = 0;
+  }
+  setTimeout(updateText, 200);
+}
+
+
 /*======================toggle wala icon navbar ko=====*/
 
-let nmuIcon = document.querySelector('#menu-icon');
+let menuIcon = document.getElementById('menu-icon');
 let navbar = document.querySelector('.navbar');
 
-menuIcon.onclick = () => {
+menuIcon.onclick= ()=> {
     menuIcon.classList.toggle('fa-xmark');
     navbar.classList.toggle('active')
 }
 /*======================scroll section=====*/
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
-window.onscroll = () >>{
+window.onscroll = ()=>{
 sections.forEach(sec => {
 let top = window.scrolly;
 let offset  = sec.offsetTop -  150;
